27f92bb611c4b42a8498e804fe73b09d08aea70e
Iftee97 <tawsifhaque97@gmail.com>
2023-11-15 18:49:03 +0600

    Refactored code to improve performance and readability.

---
+++ app/dashboard/products/loading.jsx
@@ -0,0 +1,14 @@
+export default function loading() {
+  return (
+    <div
+      style={{
+        minHeight: '70vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      loading products...
+    </div>
+  )
+}
