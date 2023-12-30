669f5225daa059b8b387315a4ffb713b69964b68
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2023-12-31 02:05:36 +0600

    added a bit of styling

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -14,7 +14,14 @@ export default function NotFound() {
       <h3 style={{ marginBottom: '1rem', fontSize: '2.25rem' }}>
         the page you're looking for doesn't exist
       </h3>
-      <Link href="/dashboard">
+      <Link 
+        href="/dashboard" 
+        style={{ 
+          border: '1px solid white', 
+          borderRadius: '9px', 
+          padding: '12px 16px', 
+        }}
+      >
         go back to dashboard
       </Link>
     </div>
